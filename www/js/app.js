8a3a6f47e77893cdf6a4fdb79b01d076ec5bad07
Wilson Farias <wilsonsf1@gmail.com>
2016-03-07 23:03:15 -0300

    Modal: incluindo botões de interação e estilo

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,11 @@
+// Indica quais objetos chamam eventos de modal
 $('.modal-trigger').leanModal();
 
+/*
+   Ao clicar num item do tipo collection-item, dentro de uma collection
+   Se não houer uma badge, ela é inicializada com 0, senão, o seu texto
+   é incrementado em 1.
+ */
 $('.collection')
   .on('click', '.collection-item', function(){
 
@@ -13,4 +19,16 @@ $('.collection')
 
   var nomeProduto = this.firstChild.textContent;
   Materialize.toast(nomeProduto + 'adicionado', 1000);
+});
+
+// Percorre os itens que possuem quantidade adicionando no resumo
+$('#confirmar').on('click', function(){
+  var texto = "";
+
+  $('.badge').parent().each(function() {
+    texto += this.firstChild.textContent + ': ';
+    texto += this.lastChild.textContent + ', ';
+  });
+
+  $('#resumo').empty().text(texto);
 });
